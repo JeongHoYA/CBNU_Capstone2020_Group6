6dc13b61fdfdac9f3d09805220b329b7cac6383f
hoya0 <hoya011196@gmail.com>
2020-05-09 22:50:22 +0900

    Make New Build with Unity's Box Collider and prefab Block

---
+++ B&C New Version/Testing map/Assets/Script/Player/MouseLook.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 500f;
+
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+
+        transform.localRotation = Quaternion.Euler(Mathf.Clamp(xRotation, -90, 90), 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
