39b9f6df3525636810b9f83f01f30b3ea3677ec4
hoya0 <hoya011196@gmail.com>
2020-05-10 18:50:19 +0900

    Optimize ChunkGeneration and Make DebugScreen

---
+++ B&C New Version/B&C Ver2 (0510)/Assets/Script/Player/PlayerControl.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.3f;
+    public LayerMask groundMask;
+
+    public float walkSpeed = 5f;
+    public float sprintSpeed = 10f;
+    public float jumpHeight = 2f;
+    public float gravity = -9.81f;
+
+    Vector3 velocity;
+
+    public bool isGrounded;
+    public bool isSprinting;
+
+
+    void Update()
+    {
+        PlayerMovement();   
+    }
+
+
+    private void PlayerMovement()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        if (Input.GetButton("Sprint"))
+        {
+            isSprinting = true;
+            controller.Move(move * sprintSpeed * Time.deltaTime);
+        }
+        else
+        {
+            isSprinting = false;
+            controller.Move(move * walkSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
