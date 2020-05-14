ee8d648119d6383eeff48076e35ddea8020a0924
hoya0 <hoya011196@gmail.com>
2020-05-14 18:30:01 +0900

    make Shader for Day/Night, optimize code, make Animation Chunk Generating function

---
+++ B&C New Version/B&C Ver2 (0514)/Assets/Script/Player/Player Control/MouseLook.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public World world;
+    public Transform playerBody;
+
+    float n = 100f;
+    float xRotation = 0f;
+
+    private void Update()
+    {
+        if(!world.inUI)
+        {
+            float mouseX = Input.GetAxis("Mouse X") * n * world.settings.mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * n * world.settings.mouseSensitivity * Time.deltaTime;
+
+            xRotation -= mouseY;
+
+            transform.localRotation = Quaternion.Euler(Mathf.Clamp(xRotation, -90, 90), 0f, 0f);
+            playerBody.Rotate(Vector3.up * mouseX);
+        }
+    }
+}
