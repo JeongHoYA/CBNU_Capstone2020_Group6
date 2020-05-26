674da32e56be284140604eeee6b004ad40b0baec
hoya0 <hoya011196@gmail.com>
2020-05-26 18:06:32 +0900

    Start RPG Tutorial for Interacting and Combat system

---
+++ _Practice_Codes(HoYA)/RPG Tutorial/RPG Tutorial (0526)/Assets/Script/CharacterAnimator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAnimator : MonoBehaviour
+{
+    NavMeshAgent agent;                                                                                             // 네비메시 에이전트 컴포넌트
+    Animator animator;                                                                                              // 에니메이터 컴포넌트
+
+    const float locomationAnimationSommthTime = 0.1f;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        float speedPercent = agent.velocity.magnitude / agent.speed;
+        animator.SetFloat("SpeedPercent", speedPercent, locomationAnimationSommthTime, Time.deltaTime);
+    }
+}
