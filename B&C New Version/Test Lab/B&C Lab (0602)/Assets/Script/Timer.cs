c247ef585a7f340d5e766146e10a32a1fecaec42
hoya0 <hoya011196@gmail.com>
2020-06-02 17:39:46 +0900

    make combat system and start make player status UI

---
+++ B&C New Version/Test Lab/B&C Lab (0602)/Assets/Script/Timer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+    public Text timer;
+    float minute, second, milliSecond;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        minute = (int)(Time.timeSinceLevelLoad / 60f);
+        second = (int)(Time.timeSinceLevelLoad % 60f);
+        milliSecond = (int)(Time.timeSinceLevelLoad * 10f) % 10;
+
+        timer.text = minute.ToString("00") + ":" + second.ToString("00") + ":" + milliSecond.ToString("0");
+    }
+}
