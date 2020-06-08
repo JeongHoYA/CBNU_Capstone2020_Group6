57e84dc250dd7e6d05b971cda658b6ec0f6d7a78
hoya0 <hoya011196@gmail.com>
2020-06-08 18:36:56 +0900

    Create ObjectStat class to optimize every Stat children class and make RandomBoxStat

---
+++ B&C New Version/Test Lab/B&C Lab (0608)/Assets/Script/World.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    public GameObject buildPlayer;
+    public GameObject clearPlayer;
+
+    public GameObject Monster;
+
+    void Start()
+    {
+        buildPlayer.SetActive(false);
+        clearPlayer.SetActive(true);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            buildPlayer.SetActive(!buildPlayer.activeSelf);
+            clearPlayer.SetActive(!clearPlayer.activeSelf);
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            Instantiate(Monster, new Vector3(10, 10, 10), new Quaternion(0, 0, 0, 0));
+        }
+    }
+}
