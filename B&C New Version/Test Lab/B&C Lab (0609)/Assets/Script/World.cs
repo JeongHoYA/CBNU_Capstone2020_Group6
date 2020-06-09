e73b7c12c41bfc4b4e193671c7a9550d5ad6a49e
hoya0 <hoya011196@gmail.com>
2020-06-09 18:19:36 +0900

    create potion, coin, clearBox function, make Day&Night with Fog and shadow

---
+++ B&C New Version/Test Lab/B&C Lab (0609)/Assets/Script/World.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    public GameObject buildPlayer;
+    public GameObject clearPlayer;
+
+    public GameObject Monster;
+    public Light directionalLight;
+
+    public Material daySky;
+    public Material nightSky;
+
+    public bool isDay;
+
+    void Start()
+    {
+        isDay = true;
+        buildPlayer.SetActive(false);
+        clearPlayer.SetActive(true);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            buildPlayer.SetActive(!buildPlayer.activeSelf);
+            clearPlayer.SetActive(!clearPlayer.activeSelf);
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            Instantiate(Monster, new Vector3(10, 10, 10), new Quaternion(0, 0, 0, 0));
+        }
+
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            isDay = !isDay;
+            if (isDay == true)
+            {
+                RenderSettings.skybox = daySky;
+                RenderSettings.fogDensity = 0f; 
+                directionalLight.transform.rotation = Quaternion.Euler(110, 0, 0);
+            }
+            else
+            {
+                RenderSettings.skybox = nightSky;
+                RenderSettings.fogDensity = 0.1f;
+                directionalLight.transform.rotation = Quaternion.Euler(50, 0, 0);
+            }
+        }
+    }
+}
