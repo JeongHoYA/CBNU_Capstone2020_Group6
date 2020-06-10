6489c3fd0185ed730c8e28c0fbc1b18493453b41
hoya0 <hoya011196@gmail.com>
2020-06-10 18:44:55 +0900

    Import Lab file to B&C

---
+++ B&C New Version/B&C Ver2 (0610)/Assets/Game/Clear/Script/Stat/Event/HitEvent.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ObjectStat))]
+public class HitEvent : MonoBehaviour
+{
+    ObjectStat stat;
+    ObjectStat otherStat;
+
+    private void Start()
+    {
+        stat = GetComponent<ObjectStat>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(this.gameObject != other.transform.root.gameObject)
+        {
+            if(other.gameObject.layer == 8)
+            {
+                otherStat = other.transform.gameObject.GetComponent<ObjectStat>();
+                if (otherStat != null)
+                {
+                    Debug.Log(other.transform.name + " attack " + this.name);
+                    stat.TakeDamage(otherStat.damage);
+                }
+            }
+            else
+            {
+                otherStat = other.transform.root.gameObject.GetComponent<ObjectStat>();
+                if (otherStat != null)
+                {
+                    Debug.Log(other.transform.root.name + " attack " + this.name);
+                    stat.TakeDamage(otherStat.damage);
+                }
+            }
+        }   
+    }
+}
