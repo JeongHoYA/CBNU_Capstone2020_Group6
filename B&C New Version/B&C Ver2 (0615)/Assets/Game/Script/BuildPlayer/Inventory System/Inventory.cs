f8f129ef7dd05a631cd8913c5cb502fd67a9d00b
hoya0 <hoya011196@gmail.com>
2020-06-15 17:11:20 +0900

    make Clear Menu UI, ClearMode UI, SpawnBlock/ClearBlock Object

---
+++ B&C New Version/B&C Ver2 (0615)/Assets/Game/Script/BuildPlayer/Inventory System/Inventory.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Inventory : MonoBehaviour
+{
+
+    public World world;
+    public GameObject slotPrefab;
+    public UIITemSlot cursorSlot;
+    public TextMeshProUGUI text;
+
+    List<ItemSlot> slots = new List<ItemSlot>();
+
+
+    private void Start()
+    {
+        for(int i = 1; i < world.blocks.Length; i++)
+        {
+            GameObject newSlot = Instantiate(slotPrefab, transform);
+            ItemSlot slot = new ItemSlot(i, newSlot.GetComponent<UIITemSlot>());
+            slot.isInventory = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (cursorSlot.itemSlot.blockID != 0)
+            text.text = world.blocks[cursorSlot.itemSlot.blockID].blockName.ToString();
+        else
+            text.text = "".ToString();
+    }
+}
---
+++ B&C New Version/B&C Ver2 (0615)/Assets/Game/Script/Stat/BoxStat/ClearBoxStat.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearBoxStat : BoxStat
+{
+    PlayerStat player;
+
+    private void Start()
+    {
+        if (World.Instance.isBuildMode == false)
+            player = GameObject.Find("ClearPlayer").GetComponent<PlayerStat>();           
+    }
+
+    public override void Die()
+    {
+        player.LevelClear = true;
+        base.Die();
+    }
+}
