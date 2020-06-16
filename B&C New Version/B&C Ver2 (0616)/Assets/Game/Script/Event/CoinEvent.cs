81e964d21de160d71e373986fc3f9cebea517f0c
hoya0 <hoya011196@gmail.com>
2020-06-16 17:30:45 +0900

    make lava/poisonGas/spiderweb/healFloor/speedupFloor block object

---
+++ B&C New Version/B&C Ver2 (0616)/Assets/Game/Script/Event/CoinEvent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinEvent : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerStat player = other.transform.root.GetComponent<PlayerStat>();
+
+        if (player != null)
+        {
+            player.coinCount++;
+            Destroy(this.gameObject);
+        }
+    }
+}
