cf033e7de63a0f2d808c5882d746ccae37aca5ef
hoya0 <hoya011196@gmail.com>
2020-06-17 15:58:22 +0900

    Add powerJumpBox and monsterSpawnBox

---
+++ B&C New Version/B&C Ver2 (0617)/Assets/Game/Script/Event/HighJumpEvent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighJumpEvent : MonoBehaviour
+{
+    public float jumpHeight = 12f;
+    float originJumpheight = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        CharController c = other.gameObject.GetComponent<CharController>();
+        PlayerStat p = other.gameObject.GetComponent<PlayerStat>();
+        originJumpheight = p.jumpHeight;
+        p.jumpHeight = jumpHeight;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        PlayerStat p = other.gameObject.GetComponent<PlayerStat>();
+        p.jumpHeight = originJumpheight;
+    }
+}
---
+++ B&C New Version/B&C Ver2 (0617)/Assets/Game/Script/Stat/CharacterStat/PlayerStat.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStat : CharacterStat
+{
+    public CharController charController;
+
+    public bool LevelClear = false;
+    public int coinCount = 0;
+
+
+    public override void Die()
+    {
+        if (!LevelClear)
+        {
+            base.Die();
+            Vector3 pos = charController.transform.position;
+            charController.transform.position = Vector3.Lerp(transform.position, transform.position + new Vector3(0, 1f, 0), 0.5f);
+        }
+    }
+
+
+    public void Clear()
+    {
+    }
+}
