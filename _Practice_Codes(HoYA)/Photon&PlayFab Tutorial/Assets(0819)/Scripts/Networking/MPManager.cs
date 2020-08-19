9d291423b06705033cdd85fede336dbbb4dab7e0
hoya0 <hoya011196@gmail.com>
2020-08-19 18:12:34 +0900

    Make Login function and Join/Create Room function

---
+++ _Practice_Codes(HoYA)/Photon&PlayFab Tutorial/Assets(0819)/Scripts/Networking/MPManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon;
+
+public class MPManager : Photon.MonoBehaviour
+{
+    public PlayfabAuth auth;                                                            // PlayFab인증 스크립트
+
+    public string gameVersion;                                                          // 게임 버전 스트링
+    public Text connectState;                                                           // 연결상태 알림창
+
+    public GameObject[] disableOnConnected;                                             // 연결시 비활성화할 오브젝트 모음
+    public GameObject[] enableOnConnected;                                              // 연결시 활성화할 오브젝트 모음
+        
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        connectState.text = PhotonNetwork.connectionStateDetailed.ToString();
+    }
+
+    public void ConnectToMaster()
+    {
+        //PhotonNetwork.connectionStateDetailed();
+        PhotonNetwork.ConnectUsingSettings(gameVersion);
+    }
+
+    public virtual void OnConnectedToMaster()
+    {
+        foreach (GameObject disable in disableOnConnected)
+            disable.SetActive(false);
+            
+        foreach (GameObject enable in enableOnConnected)
+            enable.SetActive(true);
+    }
+
+    public void CreatorJoin()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public virtual void OnPhotonRandomJoinFailed()
+    {
+        RoomOptions rm = new RoomOptions
+        {
+            MaxPlayers = 4,
+            isVisible = true
+        };
+
+        int rndID = Random.Range(0, 3000);
+        PhotonNetwork.CreateRoom("Default: " + rndID, rm, TypedLobby.Default);
+    }
+}
