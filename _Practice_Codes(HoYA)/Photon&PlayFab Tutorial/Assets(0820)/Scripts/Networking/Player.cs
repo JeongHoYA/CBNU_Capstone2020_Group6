2dc64ca521a8640d6d5f036cbb34f18ea3b9f2ce
hoya0 <hoya011196@gmail.com>
2020-08-20 17:52:52 +0900

    make movable Player object

---
+++ _Practice_Codes(HoYA)/Photon&PlayFab Tutorial/Assets(0820)/Scripts/Networking/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+
+public class Player : Photon.PunBehaviour
+{
+    new Rigidbody rigidbody;
+
+    private float x, z;
+
+    public float speed = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+
+        if (photonView.isMine)
+        {
+            
+        }
+        else
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(photonView.isMine)
+        {
+            x = Input.GetAxisRaw("Horizontal");
+            z = Input.GetAxisRaw("Vertical");
+        }
+        else
+        {
+
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(photonView.isMine)
+        {
+            rigidbody.AddForce(new Vector3(x, 0, z) * speed);
+        }
+    }
+}
